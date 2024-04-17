eca5d5b6980cc9735664652f5215b40ef76cf676
Ágata Meireles Carvalho <84482172+agatameireles11@users.noreply.github.com>
2024-04-17 20:38:33 -0300

    Add files via upload

---
+++ src/main/java/com/mycompany/diffiehellman/DiffieHellman.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.diffiehellman;
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.Scanner;
+
+/**
+ *
+ * @author agata
+ */
+public class DiffieHellman {
+
+    private static final BigInteger ONE = BigInteger.ONE;
+    private static final SecureRandom random = new SecureRandom();
+
+    // Função para gerar um número aleatório entre 2 e (p-1)
+    private static BigInteger generateRandomNumber(BigInteger p) {
+        BigInteger num;
+        do {
+            num = new BigInteger(p.bitLength(), random);
+        } while (num.compareTo(ONE) <= 0 || num.compareTo(p.subtract(ONE)) >= 0);
+        return num;
+    }
+
+    public static void main(String[] args) {
+
+        BigInteger p, g, a, b, keyA, keyB, sharedKeyA, sharedKeyB;
+        Scanner ler = new Scanner(System.in);
+        
+        /*
+        p = 101333;
+        g = 2;
+        a = 254;
+        b = 555;
+        */
+
+        System.out.println("Entre com valor de P: ");
+        p = ler.nextBigInteger();
+
+        System.out.println("Entre com valor de G: ");
+        g = ler.nextBigInteger();
+
+        System.out.println("Entre com valor da chave privada de A: ");
+        a = ler.nextBigInteger();
+
+        System.out.println("Entre com valor da chave privada de B: ");
+        b = ler.nextBigInteger();
+
+        //gera as chaves publicas
+        keyA = g.modPow(a, p);
+        keyB = g.modPow(b, p);
+
+        //Faz a troca
+        sharedKeyA = keyB.modPow(a, p);
+        sharedKeyB = keyA.modPow(b, p);
+
+        // Verificação se as chaves compartilhadas são iguais
+        if (sharedKeyA.equals(sharedKeyB)) {
+            System.out.println("Chaves compartilhadas são iguais: " + sharedKeyA);
+            System.out.println("P: " + p + "\nG: " + g + "\nChave privada de A: " + a + "\nChave Privada de B: " + b + "\nChave Pública de A: " + keyA + "\nChave Pública de B: " + keyB + "\n");
+        } else {
+            System.out.println("Erro: Chaves compartilhadas são diferentes: " + sharedKeyA + " e " + sharedKeyB);
+            System.out.println("P: " + p + "\nG: " + g + "\nChave privada de A: " + a + "\nChave Privada de B: " + b + "\nChave Pública de A: " + keyA + "\nChave Pública de B: " + keyB + "\n");
+        }
+
+    }
+
+}
